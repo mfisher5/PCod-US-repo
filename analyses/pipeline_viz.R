7cb1a66044a5448276afd9bfcd13e0f38cb509a6
mfisher5@uw.edu <mfisher5@uw.edu>
2017-03-21 16:50:13 -0700

    pipeline viz-ustacks

---
+++ analyses/pipeline_viz.R
@@ -0,0 +1,43 @@
+setwd("D:/Pacific cod/DataAnalysis/PCod-US-repo/analyses")
+AD06<- read.delim("D:/Pacific cod/DataAnalysis/PCod-US-repo/stacks/AD06_001.alleles.tsv.gz", header=FALSE, comment.char="#")
+
+# find read depth of each locus
+loci_list <- unique(AD06$V3)
+depth_list <- c()
+n.loci <- length(loci_list)
+for(locus in loci_list) {
+  indices <- which(AD06$V3 == locus)
+  depth <- 0
+  for(i in indices) {
+    depth <- depth + AD06$V6[i]
+  }
+  depth_list <- c(depth_list,depth)
+}
+boxplot(depth_list)
+title(main = "Distribution of Loci Read Depth (all)", ylab = "Number of Reads", xlab = "AD06_001")
+depth_list_small <- depth_list[depth_list < 1000]
+boxplot(depth_list_small)
+title(main = "Distribution of Loci Read Depth (<1000)", ylab = "Number of Reads", xlab = "AD06_001")
+
+
+# check to make sure that the depth is truncated at lowest allowed read depth (here, 10)
+length(which(depth_list < 10))
+
+
+#find number of haplotypes per loci
+
+
+
+
+#find distribution of haplotype depth
+boxplot(AD06$V6)
+title(main = "Distribution of Haplotype Read Depth (all)", ylab = "Number of Reads")
+
+boxplot(AD06$V6[AD06$V6 < 1000])
+title(main = "Distribution of Haplotype Read Depth (all)", ylab = "Number of Reads")
+
+# check read depths of haplotypes
+# ----What percentage of haplotypes have a read depth of less than 5?
+length(which(AD06$V6 < 5))/length(AD06$V6)
+# ----What percentage of haplotypes have a read depth of less than 10?
+length(which(AD06$V6 < 10))

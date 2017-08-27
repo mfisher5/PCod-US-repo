7bb7701c84ba052505ae5ef1ad7cec7eb2de0ba3
mfisher5@uw.edu <mfisher5@uw.edu>
2017-08-26 18:37:25 -0700

    PWStest set, radtags and bowtie alignment

---
+++ scripts/RefGenome_BOWTIEalign_batch10.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+bowtie -q -v 3 -norc --sam /mnt/hgfs/PacificCod/DataAnalysis/PCod-Compare-repo/refgenome/batch_7_refgenome /media/mfisher5/NewVolume/Mary/Stacks/PCod-US-repo/samplesT92/PWS12_001.fq PWS12_001.sam
+bowtie -q -v 3 -norc --sam /mnt/hgfs/PacificCod/DataAnalysis/PCod-Compare-repo/refgenome/batch_7_refgenome /media/mfisher5/NewVolume/Mary/Stacks/PCod-US-repo/samplesT92/PWS12_004.fq PWS12_004.sam
+bowtie -q -v 3 -norc --sam /mnt/hgfs/PacificCod/DataAnalysis/PCod-Compare-repo/refgenome/batch_7_refgenome /media/mfisher5/NewVolume/Mary/Stacks/PCod-US-repo/samplesT92/PWS12_007.fq PWS12_007.sam
+bowtie -q -v 3 -norc --sam /mnt/hgfs/PacificCod/DataAnalysis/PCod-Compare-repo/refgenome/batch_7_refgenome /media/mfisher5/NewVolume/Mary/Stacks/PCod-US-repo/samplesT92/PWS12_008.fq PWS12_008.sam
+bowtie -q -v 3 -norc --sam /mnt/hgfs/PacificCod/DataAnalysis/PCod-Compare-repo/refgenome/batch_7_refgenome /media/mfisher5/NewVolume/Mary/Stacks/PCod-US-repo/samplesT92/PWS12_012.fq PWS12_012.sam
+bowtie -q -v 3 -norc --sam /mnt/hgfs/PacificCod/DataAnalysis/PCod-Compare-repo/refgenome/batch_7_refgenome /media/mfisher5/NewVolume/Mary/Stacks/PCod-US-repo/samplesT92/PWS12_013.fq PWS12_013.sam
+bowtie -q -v 3 -norc --sam /mnt/hgfs/PacificCod/DataAnalysis/PCod-Compare-repo/refgenome/batch_7_refgenome /media/mfisher5/NewVolume/Mary/Stacks/PCod-US-repo/samplesT92/PWS12_019.fq PWS12_019.sam
+bowtie -q -v 3 -norc --sam /mnt/hgfs/PacificCod/DataAnalysis/PCod-Compare-repo/refgenome/batch_7_refgenome /media/mfisher5/NewVolume/Mary/Stacks/PCod-US-repo/samplesT92/PWS12_020.fq PWS12_020.sam
+bowtie -q -v 3 -norc --sam /mnt/hgfs/PacificCod/DataAnalysis/PCod-Compare-repo/refgenome/batch_7_refgenome /media/mfisher5/NewVolume/Mary/Stacks/PCod-US-repo/samplesT92/PWS12_025.fq PWS12_025.sam
+bowtie -q -v 3 -norc --sam /mnt/hgfs/PacificCod/DataAnalysis/PCod-Compare-repo/refgenome/batch_7_refgenome /media/mfisher5/NewVolume/Mary/Stacks/PCod-US-repo/samplesT92/PWS12_031.fq PWS12_031.sam
+bowtie -q -v 3 -norc --sam /mnt/hgfs/PacificCod/DataAnalysis/PCod-Compare-repo/refgenome/batch_7_refgenome /media/mfisher5/NewVolume/Mary/Stacks/PCod-US-repo/samplesT92/PWS12_033.fq PWS12_033.sam
+bowtie -q -v 3 -norc --sam /mnt/hgfs/PacificCod/DataAnalysis/PCod-Compare-repo/refgenome/batch_7_refgenome /media/mfisher5/NewVolume/Mary/Stacks/PCod-US-repo/samplesT92/PWS12_041.fq PWS12_041.sam
+bowtie -q -v 3 -norc --sam /mnt/hgfs/PacificCod/DataAnalysis/PCod-Compare-repo/refgenome/batch_7_refgenome /media/mfisher5/NewVolume/Mary/Stacks/PCod-US-repo/samplesT92/PWS12_054.fq PWS12_054.sam
+bowtie -q -v 3 -norc --sam /mnt/hgfs/PacificCod/DataAnalysis/PCod-Compare-repo/refgenome/batch_7_refgenome /media/mfisher5/NewVolume/Mary/Stacks/PCod-US-repo/samplesT92/PWS12_055.fq PWS12_055.sam
+bowtie -q -v 3 -norc --sam /mnt/hgfs/PacificCod/DataAnalysis/PCod-Compare-repo/refgenome/batch_7_refgenome /media/mfisher5/NewVolume/Mary/Stacks/PCod-US-repo/samplesT92/PWS12_056.fq PWS12_056.sam
+bowtie -q -v 3 -norc --sam /mnt/hgfs/PacificCod/DataAnalysis/PCod-Compare-repo/refgenome/batch_7_refgenome /media/mfisher5/NewVolume/Mary/Stacks/PCod-US-repo/samplesT92/PWS12_057.fq PWS12_057.sam
+bowtie -q -v 3 -norc --sam /mnt/hgfs/PacificCod/DataAnalysis/PCod-Compare-repo/refgenome/batch_7_refgenome /media/mfisher5/NewVolume/Mary/Stacks/PCod-US-repo/samplesT92/PWS12_064.fq PWS12_064.sam
+bowtie -q -v 3 -norc --sam /mnt/hgfs/PacificCod/DataAnalysis/PCod-Compare-repo/refgenome/batch_7_refgenome /media/mfisher5/NewVolume/Mary/Stacks/PCod-US-repo/samplesT92/PWS12_066.fq PWS12_066.sam
+bowtie -q -v 3 -norc --sam /mnt/hgfs/PacificCod/DataAnalysis/PCod-Compare-repo/refgenome/batch_7_refgenome /media/mfisher5/NewVolume/Mary/Stacks/PCod-US-repo/samplesT92/PWS12_069.fq PWS12_069.sam
+bowtie -q -v 3 -norc --sam /mnt/hgfs/PacificCod/DataAnalysis/PCod-Compare-repo/refgenome/batch_7_refgenome /media/mfisher5/NewVolume/Mary/Stacks/PCod-US-repo/samplesT92/PWS12_076.fq PWS12_076.sam
+bowtie -q -v 3 -norc --sam /mnt/hgfs/PacificCod/DataAnalysis/PCod-Compare-repo/refgenome/batch_7_refgenome /media/mfisher5/NewVolume/Mary/Stacks/PCod-US-repo/samplesT92/PWS12_077.fq PWS12_077.sam
+bowtie -q -v 3 -norc --sam /mnt/hgfs/PacificCod/DataAnalysis/PCod-Compare-repo/refgenome/batch_7_refgenome /media/mfisher5/NewVolume/Mary/Stacks/PCod-US-repo/samplesT92/PWS12_078.fq PWS12_078.sam
+bowtie -q -v 3 -norc --sam /mnt/hgfs/PacificCod/DataAnalysis/PCod-Compare-repo/refgenome/batch_7_refgenome /media/mfisher5/NewVolume/Mary/Stacks/PCod-US-repo/samplesT92/PWS12_132.fq PWS12_132.sam
+bowtie -q -v 3 -norc --sam /mnt/hgfs/PacificCod/DataAnalysis/PCod-Compare-repo/refgenome/batch_7_refgenome /media/mfisher5/NewVolume/Mary/Stacks/PCod-US-repo/samplesT92/PWS12_134.fq PWS12_134.sam
---
+++ scripts/RefGenome_BOWTIEalign_genshell_barcodefile.py
@@ -0,0 +1,52 @@
+###### Generate Shell Script to Align all FastQ Data Files to BOWTIE ref genome ######
+
+## MF 3/9/2017
+## Edited 8/26/2017 to take barcode file as sample input file
+
+
+
+## At Command Line: python cstacks_populations_genShell_3-8 ARG1 ARG2 ARG3 ARG4
+##---- ARG1 = complete sample list file
+##---- ARG2 = relative path to bowtie ref database, including file name without filetype suffix
+##---- ARG3 = relative path to stacks fastq files, output from process_radtags
+##---- ARG4 = batch #
+
+## IF fastq files are gzipped, you need to paste in the following code into the shell file: 
+#cd ../samplesT92
+#echo "finding all gzipped 'tags' files"
+#tags_file_array="$(find . -name '*.fq')"
+
+
+#echo "unzipping all fastq files"
+#for file in $tags_file_array
+#do
+#	echo $file
+#	gzip -d $file
+#	echo "file unzipped"
+#done
+
+#cd ../scripts
+
+############################################################################
+
+
+import sys
+sampfilename = sys.argv[1]
+path = sys.argv[2]
+fastq_path = sys.argv[3]
+batch = sys.argv[4]
+
+samples = open(sampfilename, "r")
+shell = open("RefGenome_BOWTIEalign_batch" + batch + ".sh", "w")
+
+shell.write("#!/bin/bash\n\n")
+
+
+
+for line in samples: 
+	sample = line.strip().split()[1]
+	newstr = "bowtie -q -v 3 -norc --sam " + path + " " + fastq_path + "/" + sample + ".fq " + sample + ".sam"
+	shell.write(newstr + "\n")
+
+samples.close()
+shell.close()
